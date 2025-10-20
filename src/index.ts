f3225a1dc4ca8130ac8275015dbee88b884b2192
nicolasances <nicolas.matteazzi@gmail.com>
2025-10-20 15:44:08 +0200

    Bedrock integration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { TotoAPIController } from "toto-api-controller";
 import { ControllerConfig } from "./Config";
 import { SayHello } from "./dlg/ExampleDelegate";
+import { PostPrompt } from "./dlg/BedrockTest";
 
 const api = new TotoAPIController("toto-ms-ex2", new ControllerConfig(), { basePath: '/ex2' });
 
+api.path('GET', '/test', new PostPrompt(), { contentType: 'application/json', noAuth: true, ignoreBasePath: false})
 api.path('POST', '/hello', new SayHello())
 
 api.init().then(() => {
